bf046325a8ddb613a61b1412f69efb4d66761cac
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-09 09:33:34 -0600

    Initial commit

---
+++ hash_practice.rb
@@ -0,0 +1,18 @@
+#vitual address book
+indiv1 = {
+  "name" => "Tim Johnson",
+  "email" => ["tim.j@cycling.com", "t.johnson@bike.org"],
+  "retired" => true
+}
+
+puts indiv1["name"]
+
+#tweet hash
+
+tweeter = {
+  "name" => "Leira Sanchez",
+  "handle" => "@mechEngSanchez",
+  "country_of_orgin" => "puerto rico",
+  "account_active" => true
+}
+puts tweeter["handle"]
